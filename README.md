87997bcb89af4418c0d248d84f0dfa1136c09dac
Leop0ld <l3opold7@gmail.com>
2018-11-01 10:43:16 +0900

    Initial commit :tada:

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hacker News Crwaler
+
+> Using Node.js, Puppeteer
+
+## TO-DO
+- [ ] Run periodically
+- [ ] Push Notification (EX. Slack, Push Bullet, etc.)
---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
+  const rows = await page.$$eval(
+    ".athing .storylink",
+    (anchors) => anchors.map((anchor) => `- [${anchor.textContent}](${anchor.href})`),
+  );
+  
+  let data = '# Hacker News\n\n';
+  data += rows.join('\n');
+  fs.writeFileSync('./generated/crawl.txt', data, 'utf8');
+
+  await browser.close();
+})();
